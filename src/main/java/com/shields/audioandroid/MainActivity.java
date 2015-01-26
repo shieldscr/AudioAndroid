257865bac80ce15c03aaee129d3d6e469dec834f
Cory Shields <shieldscr88@gmail.com>
2015-01-25 21:13:41 -0500

    Added Mockito and recodButton

--- src/main/java/com/shields/audioandroid/MainActivity.java
+++ src/main/java/com/shields/audioandroid/MainActivity.java
@@ -2,14 +2,33 @@ package com.shields.audioandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
-import com.shields.R;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.shields.R;
 
 public class MainActivity extends Activity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-  }
+    Button recordButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        recordButton = (Button) this.findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecording();
+            }
+        });
+    }
+
+    public boolean startRecording() {
+        Toast.makeText(getBaseContext(), "Recording", Toast.LENGTH_SHORT).show();
+
+        return false;
+    }
 }
--- src/test/java/com/shields/activity/MainActivityRobolectricTest.java
+++ src/test/java/com/shields/activity/MainActivityRobolectricTest.java
@@ -13,12 +13,14 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 @Config(manifest = "./src/main/AndroidManifest.xml", emulateSdk = 21)
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityRobolectricTest {
 
-    Activity mainActivity = null;
+    MainActivity mainActivity = null;
 
     public MainActivityRobolectricTest() {
         mainActivity = Robolectric.buildActivity(MainActivity.class).create().get();
@@ -33,6 +35,13 @@ public class MainActivityRobolectricTest {
     public void canGetRecordButton() {
         Button recordButton = (Button) mainActivity.findViewById(R.id.recordButton);
         assertTrue(recordButton != null);
+    }
+
+    @Test
+    public void whenRecordButtonIsClickedThenRecordingIsStarted() {
+        Button recordButton = (Button) mainActivity.findViewById(R.id.recordButton);
+        recordButton.performClick();
+        assertTrue(mainActivity.startRecording());
     }
 
 }
