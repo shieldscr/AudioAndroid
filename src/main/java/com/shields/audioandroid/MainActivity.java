5eb5564c3d8cb3f948dec961fc2de1daf4bf69cc
Cory Shields <shieldscr88@gmail.com>
2015-01-25 21:38:13 -0500

    Fixed toast message for play

--- src/main/java/com/shields/audioandroid/MainActivity.java
+++ src/main/java/com/shields/audioandroid/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends Activity {
     }
 
     public boolean play() {
-        Toast.makeText(getBaseContext(), "Recording Stopped", Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), "Play", Toast.LENGTH_LONG).show();
         return true;
     }
 }
