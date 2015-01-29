6cf7d4da0c5273c622b7fbf52f21ae3224fe436b
Cory Shields <shieldscr88@gmail.com>
2015-01-28 22:33:30 -0500

    Refactored test and new failing test until mock injection

--- src/test/java/com/shields/activity/MainActivityRobolectricTest.java
+++ src/test/java/com/shields/activity/MainActivityRobolectricTest.java
@@ -2,9 +2,11 @@ package com.shields.activity;
 
 import android.widget.Button;
 
+import com.shields.AudioIOUtility;
 import com.shields.R;
 import com.shields.audioandroid.MainActivity;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -12,15 +14,21 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @Config(manifest = "./src/main/AndroidManifest.xml", emulateSdk = 21)
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityRobolectricTest {
 
-    MainActivity mainActivity = null;
+    private MainActivity mainActivity;
+    private Button recordButton;
 
-    public MainActivityRobolectricTest() {
+    @Before
+    public void setUp() {
         mainActivity = Robolectric.buildActivity(MainActivity.class).create().get();
+        recordButton = (Button) mainActivity.findViewById(R.id.recordButton);
     }
 
     @Test
@@ -30,8 +38,15 @@ public class MainActivityRobolectricTest {
 
     @Test
     public void recordButtonExists() {
-        Button recordButton = (Button) mainActivity.findViewById(R.id.recordButton);
         assertTrue(recordButton != null);
+    }
+
+    @Test
+    public void whenRecordButtonIsClickedThenAudioIOUtilityStartsRecording() {
+        //Need to inject a mock into MainActivity...
+        AudioIOUtility mockAudioIOUtility = mock(AudioIOUtility.class);
+        verify(mockAudioIOUtility, times(1)).startRecording(mainActivity);
+        recordButton.performClick();
     }
 
     @Test
