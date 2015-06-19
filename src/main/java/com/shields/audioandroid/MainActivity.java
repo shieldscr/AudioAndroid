00331548d4995b496e12c363ca66a9b8d9171fa4
Cory Shields <shieldscr88@gmail.com>
2015-06-19 10:22:24 -0400

    spike for square button

--- src/main/java/com/shields/audioandroid/MainActivity.java
+++ src/main/java/com/shields/audioandroid/MainActivity.java
@@ -1,8 +1,9 @@
 package com.shields.audioandroid;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
-import android.widget.Button;
+import com.dd.CircularProgressButton;
 
 import com.shields.R;
 
@@ -13,17 +14,32 @@ public class MainActivity extends BaseActivity {
     @Inject
     AudioIOUtilityInterface audioIOUtilityInterface;
 
-    private Button recordButton;
+    private CircularProgressButton recordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        recordButton = (Button) this.findViewById(R.id.recordButton);
+        recordButton = (CircularProgressButton) this.findViewById(R.id.recordButton);
+
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final long length_in_milliseconds = 8000;
+                CountDownTimer timer = new CountDownTimer(length_in_milliseconds, 1) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        long progress = (millisUntilFinished / 100);
+                        recordButton.setProgress((int) progress);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        recordButton.setProgress(100);
+                    }
+                }.start();
+
                 audioIOUtilityInterface.startRecording(getApplicationContext());
             }
         });
